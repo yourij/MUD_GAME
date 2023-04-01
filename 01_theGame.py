3b5574b7b62c8a27b3996ca539f078528301b0d1
Yourij Skobolev <yourij.pl@gmail.com>
2023-04-01 23:43:31 +0200

    walk() / grid system detect / gameFieldDesc

--- 01_theGame.py
+++ 01_theGame.py
@@ -33,7 +33,7 @@ class NPC:
         self.HP = HP                # int, health points (0:100) where 0 = dead
         self.immortal = immortal    # boolean, if True = can not be dead
         self.isFound = isFound      # boolean, if True = visible to player and 'walks' with him (so maybe grid should be unlocked?)
-class Player:
+class Player:   # used in fight(), not used (yet) in walk()
     def __init__(self, appearance, name, grid, location, HP, immortal, hpBonus ) -> None:
         self.apperance = appearance # string, long description
         self.name = name            # string, short (eg. default 'Player1' or anything)
@@ -42,7 +42,7 @@ class Player:
         self.HP = HP                # int, health points (0:100) where 0 = dead
         self.immortal = immortal    # boolean, if True = can not be dead
         self.hpBonus = hpBonus      # int, eg. hpBonus=(-10) or hpBonus=20
-class Item:
+class Item: # currently hiddenKey that player has to find
     def __init__(self, appearance, name, grid, location, isFound) -> None:
         self.apperance = appearance # string, long description
         self.name = name            # string, short (Inkeeper, Princess, Witch...)
@@ -103,58 +103,91 @@ def fight(PlayerHP,OpponentHP, PlayerHPbonus, OpponentHPbonus,playerRand, oppone
     return(result)                      # result gives new HP for player
 def talk():     # (future) talking with NPCs
     print('talk function')
-def walk(input_direction):     # (IN PROGRESS...) moving aroung the grid
-    print('walk function',playerPos[0])
-
+# TO DO LIST (walk):
+# 1) use retrieved key from gameFields{} to present on screen
+# 2) define something more useful in else: for bad user input
+# 3) question - to be checked. Currently it does not return anything as playerPos is global variable... is it ok like this?
+def walk(input_direction):     # moving aroung the grid, gets input from user (north-south-east-west direction)
+    # print('declared movement input: ',input_direction)
+    # print('playerPos at start of walk()',playerPos)
+    # reads the current position to check where can player go:
+    current_grid=[currentPlace for currentPlace, currentGrid in gameFields.items() if currentGrid==playerPos] # searching values in dict:gameFields{} to get the key (field name) 
+    # print("Before the movement: ",current_grid)
     if (input_direction == 'N' or input_direction == 'n'):
-        playerPos[1]+=1     # works ok, increasing the Y coordinate
-        print('playerPos: ',playerPos,'grid: ',[currentPlace for currentPlace, currentGrid in gameFields.items() if currentGrid==playerPos])        # kurwa nie wiem jak ale to działa jeśli ==[1,1] / ale już currentGrid==playerPos troche mniej
-        print('you moved North with use of walk()')
+        time.sleep(sleepValue)
+        playerPos[1]=playerPos[1]+1     # increase the Y coordinate by 1
+        time.sleep(sleepValue)  
+        print('Kierujesz się na północ...')
     elif (input_direction == 'S' or input_direction == 's'):
-        playerPos[1]-=1
-        print(playerPos)
-        print('you moved South with use of walk()')
+        time.sleep(sleepValue)
+        playerPos[1]=playerPos[1]-1 # decrease the Y coordinate by 1
+        time.sleep(sleepValue)
+        print('Kierujesz się na południe...')
     elif (input_direction == 'E' or input_direction == 'e'):
-        playerPos[0]+=1
-        print(playerPos)
-        print('you moved East with use of walk()')
+        time.sleep(sleepValue)
+        playerPos[0]=playerPos[0]+1 # increase the X coordinate by 1
+        time.sleep(sleepValue)
+        print('Kierujesz się na wschód...')
     elif (input_direction == 'W' or input_direction == 'w'):
-        playerPos[0]-=1
-        print(playerPos)
-        print('you moved West with use of walk()')    
+        time.sleep(sleepValue)
+        playerPos[0]=playerPos[0]-1 # decrease the X coordinate by 1
+        time.sleep(sleepValue)
+        print('Kierujesz się na zachód...')    
     else:
-        print('walk() says bye!')
+        print('Ech... Wciśnij "N", jeśli chcesz iść na północ, "S" gdy na południe, "W" poprowadzi Cię na zachód, "E" prowadzi Cię na wchód')
+    time.sleep(sleepValue)
+    current_grid=[currentPlace for currentPlace, currentGrid in gameFields.items() if currentGrid==playerPos]
+    return(current_grid)
 def pickUp():   # (future) pick up an object function
     print('pickUp function')
 def enter():    # (future) entering the buildings
     print('enter function')
 
 # following should be moved to a separate file I guess... 
+
+
 hillsDesc='Skaliste wzgórza wapienne, brak roślinności jeśli nie liczyć skąpych, na wpół uschniętych badyli wystających spośród skał'
 riverValeyDesc='Dolina dzikiej rzeki. Wartki nurt z poszarpanymi brzegami. Zakole rzeki wyglądają na obiecujące łowisko.'
 swampDesc='Bagna, bardzo niebezpieczne miejsce. Rzekłbym nawet, że dość wciągające... Wycofaj się albo...'
-marschDesc='Mokradła, wilgotne i nieprzyjemne miejsce. Lepiej się tu nie zapuszczać, no chyba że się nie boisz.'
+marshDesc='Mokradła, wilgotne i nieprzyjemne miejsce. Lepiej się tu nie zapuszczać, no chyba że się nie boisz.'
 meadowDesc='Pełno tu pachnących ziół o odurzającym zapachu. Równinę przerzynają drogi białe i trochę zieleniejące od z rzadka porastającej je trawy;\n ku nim, niby strumienie ku rzekom, przybiegają z pól miedze, całe błękitne od bławatków, żółte od kamioły, różowe od dzięcieliny i smółek.\nZ obu stron każdej drogi szerokim pasem bieleją bujne rumianki i wyższe od nich kwiaty marchewnika, słają się w trawach fioletowe rohule, żółtymi gwiazdkami świecą brodawniki i kurze ślepoty.\nNienawidziłeś Orzeszkowej szczerze od dziecka. Do dziś na samo wspomnienie tego bełkotu zbiera Ci się na wymioty.'
 dessertDesc='Pustynia, piaszczysta i nudna jak flaki z olejem. Z tym, że flaki z olejem zwykle nie występują na pustyni. No, chyba że ktoś zbyt długo na niej przebywa.'
 thickForrestDesc='Gęsty las, mroczne miejsce. W sam raz dla wiedźm. Albo dzików. Albo uganiających się za nimi Galami. Rzymian nie stwierdzono.'
 wildernessDesc='Las, gdzie dzikie zwierzęta czują się najlepiej. O na przykład takie, jak ten urodzy, półtonowy niedźwiedź stojący 20 metrów na prost... 15 metrów... 10 metrów... Serio? Dalej to czytasz?'
 villageDesc='Wioska, po środku karczma. Krowa robi "Muuuu...".'
+
 playerDesc='Mięśniak z kozikiem. Półmetrowej długości kozikiem.'
 playerName='Stefan'
+
 monsterDesc='Cuchnące bydlę. Tymczasowe, póki czegoś nie wymyślę cuchnące bydlę.'
 monsterName='Arghargor, dla przyjaciół Pikuś'
 
+hiddenKeyDesc='Klucz jak klucz. Dobry do lania wosku'
+hiddenKeyName='Kluczyk'
+
 # dict mapping the locations to grid system
-gameFields={'hills'         :[0,0],              #   grid coordinates [x,y]
-            'riverValey'    :[1,0],              #
-            'swamp'         :[2,0],              #   [0,2]   [1,2]   [2,2]
-            'marsch'        :[2,1],              #
-            'meadow'        :[1,1],              #   [0,1]   [1,1]   [2,1]
-            'dessert'       :[0,1],              #
-            'thickForrest'  :[2,2],              #   [0,0]   [1,0]   [2,0]
-            'wilderness'    :[1,2],              #
-            'village'       :[0,2]               #   game starts in the middle [1,1]
+gameFields ={'hills'                :[0,0],             #   grid coordinates [x,y]
+             'riverValey'           :[1,0],             #
+             'swamp'                :[2,0],             #   [0,2]   [1,2]   [2,2]
+             'marsh'                :[2,1],             #
+             'meadow'               :[1,1],             #   [0,1]   [1,1]   [2,1]
+             'dessert'              :[0,1],             #
+             'thickForrest'         :[2,2],             #   [0,0]   [1,0]   [2,0]
+             'wilderness'           :[1,2],             #
+             'village'              :[0,2]              #   game starts in the middle [1,1]
             }
+# dict mapping the field descriptions to grid system
+gameFieldsDesc={ hillsDesc          :[0,0],             #
+                 riverValeyDesc     :[1,0],             #
+                 swampDesc          :[2,0],             #
+                 marshDesc          :[2,1],             #
+                 meadowDesc         :[1,1],             #
+                 dessertDesc        :[0,1],             #
+                 thickForrestDesc   :[2,2],             #
+                 wildernessDesc     :[1,2],             #
+                 villageDesc        :[0,2]              #
+            }
+
 
 # class Field(...) instances
 # instance_name = Field (0,     1,      2,    3,     4,                 5,              6                           )
@@ -165,38 +198,50 @@ gameFields={'hills'         :[0,0],              #   grid coordinates [x,y]
 # 4 = long text description to be printed each time player appears here
 # 5 = instance short name, to be used somehow with walk() function to describe movement
 # 6 = grid system
-hills = Field           (True,  False, True,  False, hillsDesc,        'hills',        gameFields['hills']          )
-riverValey = Field      (True,  False, True,  True,  riverValeyDesc,   'riverValey',   gameFields['riverValey']     )
-swamp = Field           (True,  False, False, True,  swampDesc,        'swamp',        gameFields['swamp']          )
-marsch = Field          (True,  True,  False, True,  marschDesc,       'marsch',       gameFields['marsch']         )
-meadow = Field          (True,  True,  True,  True,  meadowDesc,       'meadow',       gameFields['meadow']         )   # GAME STARTS HERE!
-dessert = Field         (True,  True,  True,  False, dessertDesc,      'dessert',      gameFields['dessert']        )
-thickForrest = Field    (False, True,  False, True,  thickForrestDesc, 'thickForrest', gameFields['thickForrest']   )
-wilderness = Field      (True,  True,  True,  True,  wildernessDesc,   'wilderness',   gameFields['wilderness']     )
-village = Field         (True,  True,  True,  False, villageDesc,      'village',      gameFields['village']        )
+hills = Field           (True,  False, True,  False, gameFieldsDesc[hillsDesc],         'hills',        gameFields['hills']          )
+riverValey = Field      (True,  False, True,  True,  gameFieldsDesc[riverValeyDesc],    'riverValey',   gameFields['riverValey']     )
+swamp = Field           (True,  False, False, True,  gameFieldsDesc[swampDesc],         'swamp',        gameFields['swamp']          )
+marsh = Field           (True,  True,  False, True,  gameFieldsDesc[marshDesc],         'marsh',        gameFields['marsh']          )
+meadow = Field          (True,  True,  True,  True,  gameFieldsDesc[meadowDesc],        'meadow',       gameFields['meadow']         )   # GAME STARTS HERE!
+dessert = Field         (True,  True,  True,  False, gameFieldsDesc[dessertDesc],       'dessert',      gameFields['dessert']        )
+thickForrest = Field    (False, True,  False, True,  gameFieldsDesc[thickForrestDesc],  'thickForrest', gameFields['thickForrest']   )
+wilderness = Field      (True,  True,  True,  True,  gameFieldsDesc[wildernessDesc],    'wilderness',   gameFields['wilderness']     )
+village = Field         (True,  True,  True,  False, gameFieldsDesc[villageDesc],       'village',      gameFields['village']        )
 
-sleepValue=.1                       # sleep value used in fight, when reprinting is done. May not be used in final game?
-playerPos = gameFields['meadow']    # initial player position           
+sleepValue=.5                       # sleep value used in fight, when reprinting is done. May not be used in final game?
+playerPos = [1,1]                   # initial player position [x,y]. It must be a list with direct values
 playerLoc = True                    # True - outside, False - entered any building
 playerHP = 100                      # initial player health points.
 playerImmortal = False              # DEFINE LATER OVERALL SYSTEM OF STATS MAINTENANCE AND PASSING
 playerHPbonus = 10                  # initial player's attack bonus
+playerRand=random.randrange(10)     # random used for fighting tests
+
 monsterPos = [1,1]                  # TEMPORARY TEST MONSTER (POSITION)
 monsterLoc = True                   # TEMPORARY TEST MONSTER (outside the building)
 monsterHP = 100                     # TEMPORARY TEST MONSTER (health points), CHANGE THIS FOR FIGHTING TESTS 
 monsterImmportal = False            # TEMPORARY TEST MONSTER (can be killed lock off if False) like before, this is for fight test only now
 monsterHPbonus = 10                 # TEMPORARY TEST MONSTER (attack bonus), needs to be designed later
-playerRand=random.randrange(10)     # random used for fighting tests
 monsterRand=random.randrange(10)    # random used for fighting tests
-# player 1 defined here (as well as dummy test monster)
+
+hiddenKeyPos = [1,2]                # key initial location on grid, must be direct value
+hiddenKeyLoc = False                # initial location=False means its inside a building (like inside a witch's hut)
+hiddenKeyFound = False              # initial value = False (player does not even see it). If =True means player has found it and may keep it and use it.
+
+# Player, NPCs, Monsters and other Objects appearing in the game definition here:
 player = Player (playerDesc, playerName, playerPos, playerLoc, playerHP, playerImmortal, playerHPbonus)
 dummyMonster = Monster(monsterDesc, monsterName, monsterPos, monsterLoc, monsterHP, monsterImmportal, monsterHPbonus)
+hiddenKey = Item(hiddenKeyDesc, hiddenKeyName, hiddenKeyPos, hiddenKeyLoc, hiddenKeyFound)
 
+print('Przybywasz na miejsce. '+meadowDesc+'\n')    # CHANGE THIS if starting position is changed. Otherwise its a direct callout of field description
 while (playerHP>0):
-    # print('\n*** TEST ODCZYTU KIERUNKU ORAZ DZIAŁANIA KLASY FIELD ***\n')
-    # print('Przybywasz na miejsce. '+meadowDesc)
-    direction=input('Znajdujesz się na polu: '+str(playerPos)+', gdzie chcesz póść dalej? [N/S/E/W]: ')
-    walk(direction)
+
+    show_grid=[current_Place for current_Place, current_Grid in gameFields.items() if current_Grid==playerPos]
+    direction=input('Znajdujesz się na polu: '+str(player.grid)+' '+str(show_grid)+', gdzie chcesz póść dalej? ')
+    walk_result=walk(direction)
+    show_desc=[x for x, current_Desc in gameFieldsDesc.items() if current_Desc==playerPos]
+    # print('Przed tobą... ',walk_result)
+    print(str(show_desc[0]))
+    
 
     # print('\n')  
     # print('\n*** TEST SYSTEMU WALKI (on hold, seems to work***\n')
@@ -209,8 +254,9 @@ while (playerHP>0):
     # playerHP=fight(playerHP, monsterHP, playerHPbonus, monsterHPbonus, playerRand, monsterRand)
     # print("\nRessurection xD")
 
-    print("You are in:",playerPos,"right now.")
+    # print("You are in:",playerPos,"right now.")     # calling directly playerPos variable works fine
+    # print("Player instance location: ",player.grid) # calling player.grid works fine as well
 
-    print('\n\n')  
+    # print('\n')  
 
 
